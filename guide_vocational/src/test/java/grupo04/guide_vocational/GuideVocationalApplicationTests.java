8b2590ee765227f144e6dac15c65fa5b029c181d
Angel <113852397+AngelOncoy@users.noreply.github.com>
2024-08-31 22:21:55 -0500

    Folder-proyect
    is the begin

---
+++ guide_vocational/src/test/java/grupo04/guide_vocational/GuideVocationalApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo04.guide_vocational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideVocationalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
